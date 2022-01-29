051e417b81d591592d1955fc4ea3983f4a19c26b
Jason-desktop <jason_mic2000@yahoo.com>
2022-01-29 14:11:12 +0530

    Connected to Firebase Auth and created signup hook

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1aikl2Q6G3weRHQFJQMpF5cGR6Esx0iQ",
+    authDomain: "mymoney-20484.firebaseapp.com",
+    projectId: "mymoney-20484",
+    storageBucket: "mymoney-20484.appspot.com",
+    messagingSenderId: "47260574081",
+    appId: "1:47260574081:web:8e7d5c02ecf1855fff0880"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+
+export { projectFirestore, projectAuth }
