3be3e062f85d67d754b469311f67c841dc425a63
Jason-desktop <jason_mic2000@yahoo.com>
2022-02-03 18:33:15 +0530

    Document Deletion

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,7 +14,9 @@ const firestoreReducer = (state, action) => {
     case 'IS_PENDING':
       return { isPending: true, document: null, success: false, error: null }
     case 'ADDED_DOCUMENT':
-      return { isPending: false, document: action.payload, success: true, error: null}
+      return { isPending: false, document: action.payload, success: true, error: null }
+    case 'DELETED_DOCUMENT':
+      return { isPending: false, document: null, success: true, error: null }  
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     default:
@@ -54,7 +56,15 @@ export const useFirestore = (collection) => {
 
   // delete a document
   const deleteDocument = async (id) => {
+    dispatch({ type: 'IS_PENDING' })
 
+    try {
+      await ref.doc(id).delete()
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: 'could not delete' })
+    }
   }
 
   useEffect(() => {
