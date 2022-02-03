cd3e4d58f54039f78561ad34f4e85d4c0124a744
Jason-desktop <jason_mic2000@yahoo.com>
2022-02-03 17:13:58 +0530

    Created timestamp datatype for firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,5 +18,8 @@ firebase.initializeApp(firebaseConfig)
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
 
+// timestamp
+const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth }
+
+export { projectFirestore, projectAuth, timestamp }
